6d665440650ee665ed1a9d6abee3801b80ac9c80
okbm <freezemoon.s@gmail.com>
2015-04-21 21:21:07 +0900

    mysqlのパスワード周りを外出し

--- flask_json/database.py
+++ flask_json/database.py
@@ -1,25 +1,6 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
-import os
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session
-from sqlalchemy.orm import sessionmaker
 from flask.ext.sqlalchemy import SQLAlchemy
-
 from flask_json import app
-
-def get_local_db_conf():
-    path = os.path.dirname(os.path.abspath(__file__))
-    local_dbauth_path = os.path.join(path, 'dbauth')
-    if not os.path.exists(local_dbauth_path):
-        return
-    with open(os.path.join(path, 'dbauth')) as f:
-        return f.readline().rstrip().split(':')
-
-def get_database(app):
-    PUBLIC_MYSQL_URI = 'mysql+pymysql://{}:{}@localhost/flask_json?charset=utf8'
-    DATABASE = PUBLIC_MYSQL_URI.format(*get_local_db_conf())
-    engine = create_engine(DATABASE, pool_recycle=3600)
-    return engine
 
 db = SQLAlchemy(app)
--- flask_json/settings.py
+++ flask_json/settings.py
@@ -1,5 +1,25 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
+import os
+from sqlalchemy import create_engine
+
+def get_local_db_conf():
+    path = os.path.dirname(os.path.abspath(__file__))
+    local_dbauth_path = os.path.join(path, 'dbauth')
+    if not os.path.exists(local_dbauth_path):
+        return
+    with open(os.path.join(path, 'dbauth')) as f:
+        return f.readline().rstrip().split(':')
+
+def get_database_url():
+    PUBLIC_MYSQL_URI = 'mysql+pymysql://{}:{}@localhost/flask_json?charset=utf8'
+    return PUBLIC_MYSQL_URI.format(*get_local_db_conf())
+
+def get_database_engine():
+    PUBLIC_MYSQL_URI = 'mysql+pymysql://{}:{}@localhost/flask_json?charset=utf8'
+    DATABASE = PUBLIC_MYSQL_URI.format(*get_local_db_conf())
+    engine = create_engine(DATABASE, pool_recycle=3600)
+    return engine
 
 DEBUG = True
-SQLALCHEMY_DATABASE_URI = "mysql+pymysql://flask_json:password@localhost/flask_json"
+SQLALCHEMY_DATABASE_URI = get_database_url()
